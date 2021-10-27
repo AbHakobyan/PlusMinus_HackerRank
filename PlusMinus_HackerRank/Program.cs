910071e74b7dbbe5ae17ef1d41cb5de4193f1984
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-27 12:26:53 +0400

    Добавьте файлы проекта.

---
+++ PlusMinus_HackerRank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusMinus_HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { -4, 3, -9, 0, 4, 1 };
+            plusMinus(list);
+        }
+
+        public static void plusMinus(List<int> arr)
+        {
+            int countp = 0;
+            int countz = 0;
+            int countn = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    countz++;
+                }
+                else if(arr[i] >= 1)
+                {
+                    countp++;
+                }
+                else
+                {
+                    countn++;
+                }
+            }
+            Console.WriteLine("{0:0.000000}",(double)countp / arr.Count);
+            Console.WriteLine("{0:0.000000}",(double)countn / arr.Count);
+            Console.WriteLine("{0:0.000000}",(double)countz / arr.Count);
+        }
+    }
+}
